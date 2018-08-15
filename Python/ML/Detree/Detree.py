1c849f79902f11992579c4aea21fb425cf63a0d9
abiggg <550153292@qq.com>
2018-08-16 01:49:13 +0800

    python detree

---
+++ Python/ML/Detree/Detree.py
@@ -0,0 +1,15 @@
+from math import log
+
+def calcShannonEnt(DataSet):
+    numEntries = len(DataSet)
+    labelCounts = {}
+    for featVec in DataSet:
+        currentLable = featVec[-1]
+        if currentLable not in labelCounts.keys():
+            labelCounts[currentLable] = 0
+    labelCounts[currentLable] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        probe = float(labelCounts[key]) / numEntries
+        shannonEnt -= probe * log(probe,2)
+    return shannonEnt
